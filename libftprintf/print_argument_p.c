1b43413ea266ebd145d5b4125baecf39602b5170
Matt Grimes <marriottgrimes@gmail.com>
2017-01-10 13:33:46 -0800

    p test

--- libftprintf/print_argument_p.c
+++ libftprintf/print_argument_p.c
@@ -6,7 +6,7 @@
 /*   By: mgrimes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/09 16:59:02 by mgrimes           #+#    #+#             */
-/*   Updated: 2016/12/09 16:59:03 by mgrimes          ###   ########.fr       */
+/*   Updated: 2017/01/09 11:24:26 by mgrimes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char			*print_argument_p(va_list deez_args, t_flag flags)
 	n = (unsigned long)va_arg(deez_args, void *);
 	str = ft_llutoa_base(n, 16);
 	flags.hash = 1;
-	flags.type = 'X';
+	flags.type = 'x';
 	str = format_unsigned(str, flags);
 	return (str);
 }
